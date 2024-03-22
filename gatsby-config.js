392945bfe8beb06b143b2329f047e6c9e9111879
Boyoti E. DEGBE <boyoti@Boyotis-MBP.ht.home>
2024-03-21 20:36:00 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Boyoti E. DEGBE`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
